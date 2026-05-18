3d7c7635156032db80652523b76745ba83d52a2c
KutraleeswaranC <Kutraleeswaranc@gmail.com>
2026-05-18 09:45:33 +0530

    radio button task completed

--- tests/0task.spec.js
+++ tests/0task.spec.js
@@ -1,8 +1,8 @@
 
 import{test, expect } from '@playwright/test';
 
-
-test("Test case 1: Positive LogIn test", async ({page}) => {
+// By : kutraleeswaran
+test.skip("Test case 1: Positive LogIn test", async ({page}) => {
 
     await page.goto(`https://practicetestautomation.com/`);
     await page.waitForTimeout(3000);
@@ -59,8 +59,8 @@ test("Test case 1: Positive LogIn test", async ({page}) => {
 
 
 })
-
-test("Test case 2: Negative username test", async ({page}) => {
+// By : kutraleeswaran
+test.skip("Test case 2: Negative username test", async ({page}) => {
 
     await page.goto(`https://practicetestautomation.com/`);
     await page.waitForTimeout(3000);
@@ -118,4 +118,154 @@ test("Test case 2: Negative username test", async ({page}) => {
 
 
 
+})
+
+//15-05-2026 -
+// Task : RadioButton & CheckBox
+
+// 1.Select any one  - Yes / No                                      By : kutraleeswaran
+test.skip("yes or no radio button", async({page}) => {
+
+    await page.goto(`https://letcode.in/radio`);
+    await page.waitForTimeout(3000);
+
+   
+    await page.locator(`//input[@id="yes" and @name="answer"]`).check();
+    //assertion: hard -assert
+    await expect(page.locator(`//input[@id="yes" and @name="answer"]`)).toBeChecked();
+
+    await page.waitForTimeout(3000);
+
+    
+});
+// 2.Cofirm you can select only one radio button ( Yes / No )        By : kutraleeswaran
+test.skip("Cofirm you can select only one radio button",async({page}) => {
+
+
+    await page.goto(`https://letcode.in/radio`);
+    await page.waitForTimeout(3000);
+
+    const yes = page.locator(`//input[@id="one"]`); // yes radio button locator
+    const no  = page.locator(`//input[@id="two"]`); // no  radio button locator
+
+    //lets check assertion
+
+    await yes.check();
+    await page.pause();
+    await expect(yes).toBeChecked();        //assertion for yes radio button is must be checked
+    await expect(no).not.toBeChecked();     //assertion for  no radio button is shouldnot checked
+
+    await no.check();
+    await page.pause();
+    await expect(no).toBeChecked();
+    await expect(yes).not.toBeChecked();
+
+    await page.pause();
+})
+// 3.Find the bug  (Yes /No)                                         By : kutraleeswaran
+test.skip("Find the bug radio button",async({page}) => {
+
+
+    await page.goto(`https://letcode.in/radio`);
+    await page.waitForTimeout(3000);
+
+    const yes = page.locator(`//input[@id="nobug"]`); // yes radio button locator
+    const no  = page.locator(`//input[@id="bug"]`); // no  radio button locator
+
+    //lets check assertion
+
+    await yes.check();
+
+    if(await yes.isChecked()){
+        await page.pause();
+        await expect(no).not.toBeChecked(); 
+
+        await no.check();
+
+        if(await no.isChecked()){
+            await page.pause();
+            await expect.soft(yes).not.toBeChecked();
+
+                console.log(`yes and no radio buttons are works. so, it's a bug`);
+        }
+        else{
+            console.log(`only 'yes' radio button works`);
+        } 
+    }
+    else{
+        console.log(`yes and no radio buttons are not working.`);
+    }
+})
+// 4.Find which one is selected ( Foo / Bar )                        By : kutraleeswaran
+test.skip("Find which one is selected radio button",async({page}) => {
+
+    await page.goto(`https://letcode.in/radio`);
+    await page.waitForTimeout(3000);
+
+    const foo = page.locator(`//input[@id="foo"]`); // yes radio button locator
+    const notfoo  = page.locator(`//input[@id="notfoo"]`); // no  radio button locator
+
+    //lets check assertion
+
+    await foo.check();
+
+    if(await foo.isChecked()){
+        //await page.pause();
+        await expect(notfoo).not.toBeChecked(); 
+
+        await notfoo.check();
+
+        if(await notfoo.isChecked()){
+            //await page.pause();
+            await expect.soft(foo).not.toBeChecked();
+
+                console.log(`notfoo radio selected.`);
+        }
+        else{
+            console.log(`only 'foo' radio button works`);
+        } 
+    }
+    else{
+        console.log(`foo and notfoo radio buttons are not working.`);
+    }
+})
+// 5.Confirm last field is disabled ( Going &  Not_going & Maybe )   By : kutraleeswaran
+test.skip("Confirm last field is disabled",async({page}) => {
+
+    await page.goto(`https://letcode.in/radio`);
+    await page.waitForTimeout(3000);
+
+    const going    = page.locator(`//input[@id="going"]`);
+    const notGoing = page.locator(`//input[@id="notG"]`);
+    const maybe    = page.locator(`//input[@id="going"]`);
+
+    await going.check();
+    await expect(going).toBeChecked();
+
+    await notGoing.check();
+    await expect(notGoing).toBeChecked();
+    await expect(going).not.toBeChecked();
+
+    if(maybe.isDisabled()){
+        console.log(`'maybe' radio button is disabled`);
+    }
+    else{
+        console.log(`'maybe' radio button is not disabled`);
+    }
+
+})
+// 6. Find if the checkbox is selected? ( Remember me )             By : kutraleeswaran
+test.only("Find if the checkbox is selected?",  async({page}) => {
+
+    await page.goto(`https://letcode.in/radio`);
+    await page.waitForTimeout(3000);
+
+    const rememberMe = page.locator(`//label[contains(text()," Remember me ")]/input`);
+    await rememberMe.check();
+
+    await expect(rememberMe).toBeChecked();
+
+    const termAccept = page.locator(`//label[contains(text()," I agree to the ")]/input`);
+    await termAccept.check();
+    await expect(termAccept).toBeChecked();
 })
---
+++ tests/radioCheckBox.spec.js
@@ -0,0 +1,53 @@
+import{test, expect} from '@playwright/test';
+
+test.skip("radioButton", async({page}) => {
+
+    await page.goto(`https://testautomationpractice.blogspot.com/`);
+    await page.waitForTimeout(3000);
+
+    await page.locator(`//input[@id="male"]`).check();
+
+    await expect(page.locator(`//input[@id="male"]`)).toBeChecked();
+
+    await expect(page.locator(`//input[@id="female"]`)).not.toBeChecked();
+    await Page.pause();
+
+    //uncheck doesn't work in radio button, it will automatically uncheck the other radio button when we check another one.
+    //Example;
+    await page.locator(`//input[@id="male"]`).uncheck();
+    
+    await expect(page.locator(`//input[@id="male"]`)).not.toBeChecked();
+
+})
+
+
+test.skip("checkBox", async({page}) => {
+
+    await page.goto(`https://testautomationpractice.blogspot.com/`);
+    await page.waitForTimeout(3000);
+
+    await page.locator(`//input[@id="sunday"]`).check();
+
+    await expect(page.locator(`//input[@id="sunday"]`)).toBeChecked();
+
+
+    await page.locator(`//input[@id="sunday"]`).uncheck();
+
+    await expect(page.locator(`//input[@id="sunday"]`)).not.toBeChecked();
+    await Page.pause();
+
+})
+
+test.only("multipleCheckBox", async({page}) => {
+
+    await page.goto(`https://testautomationpractice.blogspot.com/`);
+    await page.waitForTimeout(3000);
+
+    let days = [`//input[@id="sunday"]`,`//input[@id="tuesday"]`,`//input[@id="thursday"]`,`//input[@id="saturday"]`];
+
+    for(let i of days){
+            await page.locator(i).check();
+    }
+    await page.pause();
+
+})
